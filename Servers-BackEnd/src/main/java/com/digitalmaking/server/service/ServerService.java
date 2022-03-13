c9fdc4d91eaecda46d08d57d41c8ce0d5ee9dfbd
Tito <50543732+DevWebJr@users.noreply.github.com>
2022-03-13 17:21:08 +0100

    implement CRUD functions on ServerServiceImplementation.

--- Servers-BackEnd/src/main/java/com/digitalmaking/server/service/ServerService.java
+++ Servers-BackEnd/src/main/java/com/digitalmaking/server/service/ServerService.java
@@ -2,12 +2,13 @@ package com.digitalmaking.server.service;
 
 import com.digitalmaking.server.model.Server;
 
+import java.io.IOException;
 import java.util.Collection;
 
 public interface ServerService {
     Collection<Server> list(int limit);
     Server create(Server server);
-    Server ping(String ipAddress);
+    Server ping(String ipAddress) throws IOException;
     Server get(Long id);
     Server update(Server server);
     Boolean delete(Long id);
--- Servers-BackEnd/src/main/java/com/digitalmaking/server/service/implementation/ServerServiceImplementation.java
+++ Servers-BackEnd/src/main/java/com/digitalmaking/server/service/implementation/ServerServiceImplementation.java
@@ -1,14 +1,21 @@
 package com.digitalmaking.server.service.implementation;
 
+import com.digitalmaking.server.enumeration.Status;
 import com.digitalmaking.server.model.Server;
 import com.digitalmaking.server.repository.ServerRepository;
 import com.digitalmaking.server.service.ServerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.io.IOException;
+import java.net.InetAddress;
 import java.util.Collection;
+
+import static com.digitalmaking.server.enumeration.Status.SERVER_DOWN;
+import static com.digitalmaking.server.enumeration.Status.SERVER_UP;
 
 @RequiredArgsConstructor
 @Service
@@ -19,32 +26,48 @@ public class ServerServiceImplementation implements ServerService {
 
     @Override
     public Collection<Server> list(int limit) {
-        return null;
+        log.info("Fetching all servers");
+        return serverRepository.findAll(PageRequest.of(0, limit)).toList();
     }
 
     @Override
     public Server create(Server server) {
-        log.info("Saving new server: ", server.getName());
-        return null;
+        log.info("Saving new server: {}", server.getName());
+        server.setImageUrl(setServerImageUrl());
+        return serverRepository.save(server);
     }
 
     @Override
-    public Server ping(String ipAddress) {
-        return null;
+    public Server ping(String ipAddress) throws IOException {
+        log.info("Pinging server IP: {}", ipAddress);
+        Server server = serverRepository.findByIpAddress(ipAddress);
+        InetAddress address = InetAddress.getByName(ipAddress);
+        server.setStatus(address.isReachable(10000) ? SERVER_UP : SERVER_DOWN);
+        serverRepository.save(server);
+        return server;
     }
 
     @Override
     public Server get(Long id) {
-        return null;
+        log.info("Fetching server by id: {}", id);
+        return serverRepository.findById(id).get();
     }
 
     @Override
     public Server update(Server server) {
-        return null;
+        log.info("Updating server: {}", server.getName());
+        server.setImageUrl(setServerImageUrl());
+        return serverRepository.save(server);
     }
 
     @Override
     public Boolean delete(Long id) {
+        log.info("Deleting server by id: {}", id);
+        serverRepository.deleteById(id);
+        return true;
+    }
+
+    private String setServerImageUrl() {
         return null;
     }
 }
